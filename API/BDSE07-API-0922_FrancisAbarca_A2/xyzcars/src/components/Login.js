5930dd46f76be24e8265f7cc2666dba78b64f0f9
pointlessduffin-21 <francisroelabarca12@gmail.com>
2023-05-23 16:24:52 +0800

    Added the Login and Register page

--- API/BDSE07-API-0922_FrancisAbarca_A2/xyzcars/src/components/Login.js
+++ API/BDSE07-API-0922_FrancisAbarca_A2/xyzcars/src/components/Login.js
@@ -1,36 +1,91 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            error_string: null,
+            success_login: null,
+            success_logout: null
+        };
+    }
+
+    handleInputChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+        // TODO: add login logic here
+    }
+
     render() {
         return (
-            <><div>
-                <h1>Login</h1>
-                <p>Login here.</p>
-            </div><form>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control"
-                            id="exampleInputEmail1" aria-describedby="emailHelp"
-                            placeholder="Enter email" />
-                        <small id="emailHelp" className="form-text text-muted">
-                            We'll never share your email with anyone else.
-                        </small>
+            <main className="page contact-us-page">
+                <section className="clean-block clean-form dark">
+                    <div className="container">
+                        <div className="block-heading">
+                            <h2 className="text-info">Sign-in</h2>
+                            <p>Come and utilize our services!</p>
+                        </div>
+
+                        {this.state.error_string && (
+                            <div className="alert alert-danger">
+                                {this.state.error_string}
+                                Click here to <Link to="/register_user"> Register</Link>
+                            </div>
+                        )}
+
+                        {this.state.success_login && (
+                            <div className="alert alert-success">
+                                {this.state.success_login}
+                                Click here to navigate to <Link to="/home"> Home</Link>
+                            </div>
+                        )}
+
+                        {this.state.success_logout && (
+                            <div className="alert alert-success">
+                                {this.state.success_logout}
+                                Click here to <Link to="/login">Login</Link>
+                            </div>
+                        )}
+
+                        {(!this.state.success_login && !this.state.success_logout) && (
+                            <form onSubmit={this.handleSubmit} className="was-validated">
+                                <div className="mb-3 mt-3">
+                                    <label htmlFor="username" className="form-label">User name:</label>
+                                    <input type="text" className="form-control" id="userName" 
+                                           placeholder="Enter username" name="username" value={this.state.username}
+                                           onChange={this.handleInputChange} required />
+                                    <div className="valid-feedback">Valid.</div>
+                                    <div className="invalid-feedback">Please fill out this field.</div>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="password" className="form-label">Password:</label>
+                                    <input type="password" className="form-control" id="password"
+                                           placeholder="Enter password" name="password" value={this.state.password}
+                                           onChange={this.handleInputChange} required />
+                                    <div className="valid-feedback">Valid.</div>
+                                    <div className="invalid-feedback">Please fill out this field.</div>
+                                </div>
+                                
+                                <br />
+                                <input type="submit" name="Login" value="Sign In" className="btn btn-primary" />
+                                <Link to="/Register" className="btn btn-primary">Register</Link>
+                            </form>
+                        )}
+                        
+                        <div style={{margin: '80px'}}></div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputPassword1">Password</label>
-                        <input type="password"
-                            className="form-control" id="exampleInputPassword1"
-                            placeholder="Password" />
-                    </div>
-                    <div className="form-group form-check">
-                        <input type="checkbox"
-                            className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label"
-                            htmlFor="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form></>
-        )
+                </section>
+            </main>
+        );
     }
 }
 
--- API/BDSE07-API-0922_FrancisAbarca_A2/xyzcars/src/components/Register.js
+++ API/BDSE07-API-0922_FrancisAbarca_A2/xyzcars/src/components/Register.js
@@ -1,50 +1,91 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            form: {
+                firstName: '',
+                lastName: '',
+                address: '',
+                username: '',
+                email: '',
+                phone: '',
+                password: '',
+                confirmPassword: ''
+            },
+        };
+    }
+
+    handleInputChange = (event) => {
+        this.setState({
+            form: {
+                ...this.state.form,
+                [event.target.id]: event.target.value,
+            }
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.form);
+        // submit the form here
+    };
+
     render() {
         return (
-            <><div>
-                <h1>Register</h1>
-                <p>Register here.</p>
-            </div><form>
-                    <div className="form-group">
-                        <label htmlFor="firstName">First Name:</label>
-                        <input type="text" className="form-control" id="firstName" placeholder="First Name" required/>
+            <main className="page contact-us-page">
+                <section className="clean-block clean-form dark">
+                    <div className="container">
+                        <div className="block-heading">
+                            <h2 className="text-info">Register</h2>
+                            <p>Come and utilize our services!</p>
+                        </div>
+
+                        <form onSubmit={this.handleSubmit} className="was-validated">
+                            <div className="form-group">
+                                <label htmlFor="firstName">First Name:</label>
+                                <input type="text" className="form-control" id="firstName" placeholder="First Name" required onChange={this.handleInputChange}/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="lastName">Last Name:</label>
+                                <input type="text" className="form-control" id="lastName" placeholder="Last Name" required onChange={this.handleInputChange}/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="address">Address:</label>
+                                <input type="text" className="form-control" id="address" placeholder="Address" required onChange={this.handleInputChange}/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="username">Username:</label>
+                                <input type="text" className="form-control" id="address" placeholder="Username" required onChange={this.handleInputChange}/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="email">Email Address:</label>
+                                <input type="text" className="form-control" id="email" placeholder="Email Address" required onChange={this.handleInputChange}/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="phone">Phone Number:</label>
+                                <input type="text" className="form-control" id="phone" placeholder="Phone Number" required onChange={this.handleInputChange}/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password:</label>
+                                <input type="password" className="form-control" id="password" placeholder="Password" required onChange={this.handleInputChange}/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="confirmPassword">Confirm Password:</label>
+                                <input type="password" className="form-control" id="confirmPassword" placeholder="Confirm Password" required onChange={this.handleInputChange}/>
+                            </div>
+                            <br />
+                            <input type="submit" value="Register" className="btn btn-primary" />
+                        </form>
+                        
+                        <div style={{margin: '80px'}}></div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="lastName">Last Name:</label>
-                        <input type="text" className="form-control" id="lastName" placeholder="Last Name" required/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="address">Address:</label>
-                        <input type="text" className="form-control" id="address" placeholder="Address" required/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="username">Username:</label>
-                        <input type="text" className="form-control" id="username" placeholder="Username" required/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">Email:</label>
-                        <input type="text" className="form-control" id="email" placeholder="Email" required/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="phone">Phone Number:</label>
-                        <input type="text" className="form-control" id="phone" placeholder="Phone" required/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password:</label>
-                        <input type="text" className="form-control" id="password" placeholder="Password" required/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Confirm Password:</label>
-                        <input type="text" className="form-control" id="password" placeholder="Password" required/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="button">Register</label>
-                        <button type="submit" className="btn btn-primary">Register</button>
-                    </div>
-                </form></>
-        )
+                </section>
+            </main>
+        );
     }
 }
 
