9aa23aae4ffc61e7a262c1bdd4152f31a0d1eb5f
pointlessduffin-21 <francisroelabarca12@gmail.com>
2023-05-26 13:02:05 +0800

    Added more items for Back-End

---
+++ API/BDSE07-API-0922_FrancisAbarca_A3/xyzcars-springboot/src/main/java/xyz/yeems214/xyzcars/Controller/AuthController.java
@@ -0,0 +1,37 @@
+package xyz.yeems214.xyzcars.Controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class AuthController {
+    @GetMapping
+    public Principal getUser(final Principal user) {
+        return user;
+    }
+
+    @GetMapping(value="/checkAuthentication")
+    public Map<String, Object> checkAuthentication() {
+        Map<String, Object> map = new HashMap<>();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null && auth.isAuthenticated()) {
+            map.put("authenticated", true);
+        } else {
+            map.put("authenticated", false);
+        }
+
+        return map;
+    }
+
+}
