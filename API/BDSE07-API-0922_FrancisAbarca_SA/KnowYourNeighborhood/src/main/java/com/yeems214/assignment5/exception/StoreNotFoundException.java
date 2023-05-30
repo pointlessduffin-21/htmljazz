0dc68bbe6e82a2e3d0c987bbea294b9adfc6cbd7
pointlessduffin-21 <francisroelabarca12@gmail.com>
2023-05-30 12:01:18 +0800

    Added files for Summative

--- API/BDSE07-API-0922_FrancisAbarca_SA/KnowYourNeighborhood/src/main/java/com/yeems214/assignment5/exception/StoreNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.yeems214.assignment5.exception;
-
-public class StoreNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-    public StoreNotFoundException(String message) {
-        super(message);
-    }
-}
--- API/BDSE07-API-0922_FrancisAbarca_SA/kyn/src/App.js
+++ API/BDSE07-API-0922_FrancisAbarca_SA/kyn/src/App.js
@@ -3,20 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/terms" element={<Terms />} />
-        </Routes>
-        <Footer />
-        </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
