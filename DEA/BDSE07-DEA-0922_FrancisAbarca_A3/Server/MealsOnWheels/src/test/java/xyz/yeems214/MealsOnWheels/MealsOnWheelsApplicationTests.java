7d6f94c50b1dd6c86c65545e0a49bfb887b9f015
pointlessduffin-21 <francisroelabarca12@gmail.com>
2023-06-13 09:12:46 +0800

    Added new files for Developing Enterprise Applications

---
+++ DEA/BDSE07-DEA-0922_FrancisAbarca_A3/Server/MealsOnWheels/src/test/java/xyz/yeems214/MealsOnWheels/MealsOnWheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.yeems214.MealsOnWheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsOnWheelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
