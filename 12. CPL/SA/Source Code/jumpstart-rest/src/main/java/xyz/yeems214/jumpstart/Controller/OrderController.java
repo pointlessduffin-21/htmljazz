5a7372f12bbb9cdb4fe15f11c971f4e94f2d89d7
pointlessduffin-21 <francisroelabarca12@gmail.com>
2023-09-14 14:54:33 +0800

    Added more report stuff

--- 12. CPL/SA/Source Code/jumpstart-rest/src/main/java/xyz/yeems214/jumpstart/Controller/OrderController.java
+++ 12. CPL/SA/Source Code/jumpstart-rest/src/main/java/xyz/yeems214/jumpstart/Controller/OrderController.java
@@ -26,41 +26,4 @@ public class OrderController {
         return orderRepository.findAll().toString();
     }
 
-    @PostMapping("/place-order")
-    public ResponseEntity<?> placeOrder(@RequestBody Order order, @RequestParam("user_id") Long userId) {
-        // Fetch the User entity using the provided ID
-        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
-        order.setUser(user);
-
-        // Validate the products
-        List<Product> products = order.getProducts();
-        if (products == null || products.isEmpty()) {
-            return new ResponseEntity<>("No products provided.", HttpStatus.BAD_REQUEST);
-        }
-
-        // Validate the quantity
-        Long quantity = order.getQuantity();
-        if (quantity == null || quantity <= 0) {
-            return new ResponseEntity<>("Invalid quantity.", HttpStatus.BAD_REQUEST);
-        }
-
-        // Calculate the total price
-        long totalPrice = products.stream()
-                .mapToLong(product -> product.getPrice() * quantity)
-                .sum();
-        order.setTotal_price(totalPrice);
-
-        // Save the order to the database
-        Order placedOrder;
-        try {
-            placedOrder = orderRepository.save(order);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Failed to place order.", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-        // Return a response
-        return new ResponseEntity<>(placedOrder, HttpStatus.CREATED);
-    }
-
-
 }
